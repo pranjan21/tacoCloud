f49254342a1b4eb704aa2e9ac46fec66c5e7a96b
Priyanka Ranjan <pranjan@tribalscale.com>
2019-07-15 11:10:56 +0400

    Added a registration form, User entity model, User Repository using CrudRepo and a UserDetailsServiceController to manager authentication with users

--- taco-cloud/src/main/java/tacos/domains/User.java
+++ taco-cloud/src/main/java/tacos/domains/User.java
@@ -1,45 +1,119 @@
 package tacos.domains;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
 
+@Data
 @Entity
-public class User {
-    private String name;
-    private String username;
-    private String password;
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    public User(String name, String username, String password) {
-        this.name = name;
-        this.password = password;
-        this.username = username;
+    private final String username;
+    private final String password;
+    private final String fullname;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phoneNumber;
+
+//    public User(String username, String password, String fullname, String street, String city, String state, String zip, String phone) {
+//        this.username = username;
+//        this.password = password;
+//        this.fullname = fullname;
+//        this.street = street;
+//        this.city = city;
+//        this.state = state;
+//        this.zip = zip;
+//        this.phoneNumber = phone;
+//    }
+
+    // Override Methods
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public User() {
-
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public String getUsername() {
-        return username;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
-    public void setName(String name) {
-        this.name = name;
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    @Override
+    public String getUsername() {
+        return username;
     }
+
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
 }
